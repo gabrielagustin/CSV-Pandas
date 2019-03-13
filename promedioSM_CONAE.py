bc8d482e1dc6a9128be19ab6a2cb4b7b718cbb11
gabrielagustin <gabiagus@gmail.com>
2019-03-13 09:21:35 -0300

    add changes

---
+++ promedioSM_CONAE.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  5 06:42:18 2018
+
+@author: gag
+"""
+
+import pandas as pd
+from os import listdir
+import os
+import numpy as np
+
+dir = "gag"
+
+
+#file = "/media/"+dir+"/TOURO Mobile/Trabajo_Sentinel_NDVI_CONAE/Modelo/mapasCreados/Etapa1/tabla_extract.csv"
+#data = pd.read_csv(file, sep=',', decimal=",")
+#
+#print data
+
+pathIN = "/home/gag/Escritorio/mediciones_sensoresHPII_160415-050715_MB/"
+pathOUT = "/home/gag/Escritorio/mediciones_sensoresHPII_160415-050715_MB/SM_CONAE_Prom/"
+
+for cosa in listdir(pathIN):
+    if os.path.splitext(cosa)[-1] == '.csv':
+        print(cosa)
+        nom = cosa[:-4] + '_Prom.csv'
+        file = pathIN + cosa
+        data = pd.read_csv(file, sep=',', decimal=",")
+#        print(data)
+#        result = data.sort(['ID_DISPOSI,N,19,0'])
+#        print(data)
+        
+#        Latitud = np.unique(result['LATITUD,N,32,10'].values)
+#        Longitud = np.unique(result['LONGITUD,N,32,10'].values)
+        data_mean_SM = (data['RSOILMOIST,N,32,10'].groupby(data['ID_DISPOSI,N,19,0']).mean())
+#        print(data_mean_SM)
+        data_mean_Ts = (data['RSOILTEMPC,N,32,10'].groupby(data['ID_DISPOSI,N,19,0']).mean())
+#        print(data_mean_Ts)
+        Latitud = (data['LATITUD,N,32,10'].groupby(data['ID_DISPOSI,N,19,0']).mean())
+        Longitud= (data['LONGITUD,N,32,10'].groupby(data['ID_DISPOSI,N,19,0']).mean())
+#        Latitud = (data['LATITUD,N,32,10'].groupby(data['ID_DISPOSI,N,19,0']).axis=0)
+#        Latitud = (groupby(data['ID_DISPOSI,N,19,0']).['LATITUD,N,32,10'])
+#        print(Latitud)
+
+        #
+        df = pd.DataFrame({'Latitud':Latitud,'Longitud':Longitud,'Ts':np.array(data_mean_Ts),'SM':data_mean_SM})
+#        print(df)
+        df.to_csv(pathOUT + nom, decimal = ",")
+        print("Archivo creado con exito!" + str(nom))
